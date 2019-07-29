37a935a6c4679f26f740abd3c05e9313d7095690
luobaijian <772009332@qq.com>
2019-07-29 23:52:32 +0800

    分类导航完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,37 @@
 // pages/index/index.js
 Page({
-data: {
-swiperList:[]
-},
-onLoad(){
-  this.getSwiperList()
-},
-getSwiperList(){
-  wx.request({
-    url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-    success: (result) => {
-      console.log(result.data.message);
-      this.setData({
-        swiperList:result.data.message
-      })
-    },
-  });
-    
-}
+  data: {
+    swiperList: [],
+    navCateList: []
+  },
+  onLoad() {
+    this.getSwiperList()
+    this.getCateList()
+  },
+  //请求轮播图数据
+  getSwiperList() {
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (result) => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      },
+    });
+
+  },
+  // 请求分类导航数据
+  getCateList() {
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success: (result) => {
+        console.log(result.data.message);
+        this.setData({
+          navCateList: result.data.message
+        })
+      },
+
+    });
+
+  }
 })
