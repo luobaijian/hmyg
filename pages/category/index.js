4aa3853659ab6ea068f17fe978cf91dfc838584a
luobaijian <772009332@qq.com>
2019-07-31 22:03:50 +0800

    把请求回来的数据存储到本地

--- pages/category/index.js
+++ pages/category/index.js
@@ -14,9 +14,34 @@ Page({
     currentIndex:0,
     scrollTop:0,
   },
-  cates:[],
+  Cates:[],
   onLoad(){
-    this.getCategoryList()
+    const cates = wx.getStorageSync("cates");
+    // 没有数据的时候，崇信发送请求，并且把数据存储到本地
+      if (!cates){
+        this.getCategoryList()
+      }
+      else{
+        // 有数据 判断数据是否过期，暂定时间超过20S，就过期
+        if(Date.now()-cates.time>20*1000){
+          this.getCategoryList()
+        }
+          // 数据没有过期 可以直接使用
+        else{
+          this.Cates=cates.data
+          let leftMenuList = this.Cates.map((v, i) => ({
+            cat_name: v.cat_name,
+            cat_id: v.cat_id
+          }))
+          let rightMenuList = this.Cates[0].children
+          this.setData({
+            leftMenuList,
+            rightMenuList
+          })
+        }
+      }
+
+    
   },
   //获取分类数据
   getCategoryList(){
@@ -25,12 +50,17 @@ Page({
     })
     .then(result=>{
       console.log(result);
-      this.cates=result;
-      let leftMenuList=this.cates.map((v,i)=>({
+
+      //把请求回来的数据赋值
+      this.Cates = result;
+      //把请求回来的数据存储在本地
+      wx.setStorageSync("cates", {time:Date.now(),data:this.Cates});
+        
+      let leftMenuList = this.Cates.map((v, i) => ({
         cat_name: v.cat_name,
         cat_id: v.cat_id
       }))
-      let rightMenuList = this.cates[0].children
+      let rightMenuList = this.Cates[0].children
       this.setData({
         leftMenuList,
         rightMenuList
@@ -40,9 +70,8 @@ Page({
 
   // 点击左侧菜单
   handleMEnuChange(e){
-    console.log(e.currentTarget.dataset.index);
     const {index}=e.currentTarget.dataset
-    let rightMenuList=this.cates[index].children
+    let rightMenuList = this.Cates[index].children
     this.setData({
       rightMenuList,
       currentIndex:index,
