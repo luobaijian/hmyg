5c4bea9859a8fd0e0329ca77e6c3340b613ce6a8
luobaijian <772009332@qq.com>
2019-07-31 21:36:06 +0800

    点击左侧菜单，右侧商品会切换，并且会到达页面顶端

--- pages/category/index.js
+++ pages/category/index.js
@@ -11,26 +11,42 @@ Page({
   data: {
     leftMenuList:[],
     rightMenuList:[],
-    currentIndex:0
+    currentIndex:0,
+    scrollTop:0,
   },
+  cates:[],
   onLoad(){
     this.getCategoryList()
   },
+  //获取分类数据
   getCategoryList(){
     request({
       url: '/categories'
     })
     .then(result=>{
       console.log(result);
-      let leftMenuList=result.map((v,i)=>({
+      this.cates=result;
+      let leftMenuList=this.cates.map((v,i)=>({
         cat_name: v.cat_name,
         cat_id: v.cat_id
       }))
-      let rightMenuList=result[0].children
+      let rightMenuList = this.cates[0].children
       this.setData({
         leftMenuList,
         rightMenuList
       })
+    })
+  },
+
+  // 点击左侧菜单
+  handleMEnuChange(e){
+    console.log(e.currentTarget.dataset.index);
+    const {index}=e.currentTarget.dataset
+    let rightMenuList=this.cates[index].children
+    this.setData({
+      rightMenuList,
+      currentIndex:index,
+      scrollTop:0
     })
   }
 
